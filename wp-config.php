1a4a47e4d951b0ab9b8f353b0946451347590856
elf <filonenko0406@gmail.com>
2019-09-08 20:57:45 +0800

    fix db setting

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'etalon_admin');
 define( 'DB_PASSWORD', '}P%BAwQ6K$PR');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db:3306');
+define( 'DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
