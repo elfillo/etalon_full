79470dc5eedee8a3d69153d21c86c12401cf4609
elf <filonenko0406@gmail.com>
2019-11-11 13:31:28 +0800

    fix slider

--- wp-content/themes/etalon/js/swiper-main.js
+++ wp-content/themes/etalon/js/swiper-main.js
@@ -1,38 +1,28 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    //spaceBetween: 30,
-    freeMode: true,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        1280: {
-            slidesPerView: 1,
-            spaceBetween: 0
-        },
+$( document ).ready(function () {
+    if(window.location.pathname === '/'){
+        var swiper_sale = new Swiper('.sale-slider', {
+            spaceBetween: 30,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    }else {
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 3,
+            //spaceBetween: 30,
+            freeMode: true,
+            loop: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                1280: {
+                    slidesPerView: 1,
+                    spaceBetween: 0
+                },
+            }
+        });
     }
-});
-
-var swiper_sale = new Swiper('.sale-slider', {
-    /*slidesPerView: 3,
-    //spaceBetween: 30,
-    freeMode: true,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        1280: {
-            slidesPerView: 1,
-            spaceBetween: 0
-        },
-    }*/
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
 });
